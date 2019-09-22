24b842a8389517e0fd4c399e4bb70c9c9b88d763
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-09-22 02:05:24 +0200

    Fix Appveyor scripts running deployment scripts on any commit

--- ci/appveyor/deploy.ps1
+++ ci/appveyor/deploy.ps1
@@ -1,6 +1,6 @@
 If ($env:APPVEYOR_REPO_TAG -eq "true") {
+    Invoke-Expression "python ci/vercheck.py"
     Invoke-Expression "twine upload --skip-existing dist/*.whl" 2>$null
-    Invoke-Expression "cargo publish --token $env:CRATES_IO_TOKEN"
 } Else {
     write-output "Not on a tag on master, won't deploy to pypi"
 }
