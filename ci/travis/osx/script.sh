15a5a62adb836a0a9a07845ac67e3b1843408acc
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-04-07 21:53:18 +0200

    Fix PyEnv not being activated on script step in OSX CI

--- ci/travis/osx/script.sh
+++ ci/travis/osx/script.sh
@@ -2,6 +2,12 @@
 
 . $(dirname $(dirname $0))/functions.sh
 
+# --- Using proper Python executable -----------------------------------------
+
+log Activating pyenv
+eval "$(pyenv init -)"
+pyenv shell $(pyenv versions --bare)
+
 # --- Test -------------------------------------------------------------------
 
 $PYTHON setup.py test
